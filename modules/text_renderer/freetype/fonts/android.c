videolan/vlc

freetype: android: process formatted text nodes
Trims filenames as they are not stored as attributes.

Also removes incorrect asprintf 0 return value handling

modules/text_renderer/freetype/fonts/android.c
@@ -84,9 +84,31 @@ static int Android_ParseFont( vlc_font_select_t *fs, xml_reader_t *p_xml,
      * We don't need all font weights. Only 400 (regular) and 700 (bold)
      */
     if( i_weight == 400 || i_weight == 700 )
-        if( asprintf( &psz_fontfile, "%s/%s", SYSTEM_FONT_PATH, psz_val ) < 0
-         || !NewFont( psz_fontfile, 0, i_flags, p_family ) )
+    {
+        /* left trim */
+        psz_val += strspn( psz_val, " \t\r\n" );
+        /* right trim */
+        size_t len = strlen( psz_val );
+        const char *psz_end = psz_val + len;
+        while( psz_end > psz_val &&
+               ( psz_end[-1] == ' ' ||
+                 psz_end[-1] == '\t' ||
+                 psz_end[-1] == '\r' ||
+                 psz_end[-1] == '\n' ) )
+            psz_end--;
+        len = psz_end - psz_val;
+
+        psz_fontfile = malloc( sizeof(SYSTEM_FONT_PATH) + 1 + len );
+        if( !psz_fontfile )
             return VLC_ENOMEM;
+        memcpy( psz_fontfile, SYSTEM_FONT_PATH, sizeof(SYSTEM_FONT_PATH) - 1 );
+        psz_fontfile[sizeof(SYSTEM_FONT_PATH) - 1] = '/';
+        memcpy( &psz_fontfile[sizeof(SYSTEM_FONT_PATH)], psz_val, len );
+        psz_fontfile[sizeof(SYSTEM_FONT_PATH) + len] = '\0';
+
+        if( !NewFont( psz_fontfile, 0, i_flags, p_family ) )
+            return VLC_ENOMEM;
+    }
 
     return VLC_SUCCESS;
 }

ijkm1234/vlc_for_xr_vlc

Message-Id: <c53d7f0295a1eba02a24794891ef2579e556793c.1638525624.git.hugo@beauzee.fr>
Message-Id: <02e86e75af30a959de75f343a268b06076acdca6.1657539375.git.thomas@gllm.fr>
Message-Id: <897c409d7d95df71f92f0680ef70d8aee6a69610.1662737834.git.dev.asenat@posteo.net>
Message-Id: <e7493498e2e4a76e68957df7f7333bfae4bc4551.1664884159.git.dev.asenat@posteo.net>
Message-Id: <f6c5bafb73b2b2e0b135638d104c3a0452edd6bb.1664886558.git.dev.asenat@posteo.net>
Message-ID: <2bbc3ab6e9689027935aaf516369714f43de95d6.1738244316.git.dcn.mcnamara@gmail.com>

modules/text_renderer/freetype/fonts/android.c
@@ -84,9 +84,31 @@ static int Android_ParseFont( filter_t *p_filter, xml_reader_t *p_xml,
      * We don't need all font weights. Only 400 (regular) and 700 (bold)
      */
     if( i_weight == 400 || i_weight == 700 )
-        if( asprintf( &psz_fontfile, "%s/%s", ANDROID_FONT_PATH, psz_val ) < 0
-         || !NewFont( psz_fontfile, 0, b_bold, b_italic, p_family ) )
+    {
+        /* left trim */
+        while( *psz_val && *psz_val <= ' ' )
+            psz_val++;
+        /* right trim */
+        size_t len = strlen( psz_val );
+        if( len > 1 )
+        {
+            const char *psz_end = psz_val + len;
+            while( psz_end > psz_val + 1 && psz_end[-1] <= ' ' )
+                psz_end--;
+            len = psz_end - psz_val;
+        }
+
+        psz_fontfile = malloc( sizeof(ANDROID_FONT_PATH) + 1 + len );
+        if( !psz_fontfile )
             return VLC_ENOMEM;
+        psz_fontfile[0] = '\0';
+        strcat( psz_fontfile, ANDROID_FONT_PATH );
+        strcat( psz_fontfile, "/" );
+        strncat( psz_fontfile, psz_val, len );
+
+        if( !NewFont( psz_fontfile, 0, b_bold, b_italic, p_family ) )
+            return VLC_ENOMEM;
+    }
 
     return VLC_SUCCESS;
 }